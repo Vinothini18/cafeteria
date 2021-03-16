2be20f2844a2ec52162bfecd915c731786168585
Vinothini18 <vino14768@gmail.com>
2021-03-16 22:53:44 +0530

    Added a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_16_144910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,26 @@ ActiveRecord::Schema.define(version: 0) do
     t.text "item_quantity"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "note"
+    t.date "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "todos", force: :cascade do |t|
     t.text "order_id"
     t.date "order_date"
     t.boolean "completed"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
