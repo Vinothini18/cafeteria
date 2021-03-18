ccb0882166808165ebffb13ace8c70e7abbecf21
Vinothini18 <vino14768@gmail.com>
2021-03-18 17:27:05 +0530

    Added Authentication to user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,7 @@ class OrdersController<ApplicationController
         id=params[:id]
         order=Order.find(id)
         render plain:order.to_pleasant_string
+    end
 
     def create
         item_name=params[:item_name]
@@ -19,8 +20,17 @@ class OrdersController<ApplicationController
         response_text="New Order is created"
         render plain:response_text
     end
-    
-    
-
-
+    def update
+        item_name=params[:item_name]
+        item_quantity=params[:item_quantity]
+        item_price =params[:item_price]
+        order.save!
+        redirect_to orders_path
+    end
+    def destroy
+        id=params[:id]
+        order=Order.find(id)
+        order.destroy
+        redirect_to orders_path
+    end
 end
